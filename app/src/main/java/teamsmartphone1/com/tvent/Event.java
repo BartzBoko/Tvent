747a6d4c9108ba3242781c672f70e2de72b317e5
BartzBoko <byang77@gatech.edu>
2016-09-21 14:07:21 -0400

    Added two class files.

---
+++ app/src/main/java/teamsmartphone1/com/tvent/Event.java
@@ -0,0 +1,8 @@
+package teamsmartphone1.com.tvent;
+
+/**
+ * Created by Bruce on 9/14/2016.
+ */
+public class Event {
+
+}
---
+++ app/src/main/java/teamsmartphone1/com/tvent/Tweet.java
@@ -0,0 +1,7 @@
+package teamsmartphone1.com.tvent;
+
+/**
+ * Created by Bruce on 9/14/2016.
+ */
+public class Tweet {
+}
