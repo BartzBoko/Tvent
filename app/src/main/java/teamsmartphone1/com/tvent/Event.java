3632102c693d1816aa8d55deef6362e9222c69fb
BartzBoko <byang77@gatech.edu>
2016-09-23 14:32:24 -0400

    JSON parsing set up. May need to edit after we get access to the events server.

--- app/src/main/java/teamsmartphone1/com/tvent/Event.java
+++ app/src/main/java/teamsmartphone1/com/tvent/Event.java
@@ -1,10 +1,20 @@
 package teamsmartphone1.com.tvent;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.HashSet;
 
 /**
  * Created by Bruce on 9/14/2016.
  */
 public class Event {
+    int id;
+    LatLng geotag;
     HashSet<Tweet> tweets;
+
+    public Event(int i, LatLng g, HashSet<Tweet> t) {
+        id = i;
+        geotag = g;
+        tweets = t;
+    }
 }
--- app/src/main/java/teamsmartphone1/com/tvent/EventList.java
+++ app/src/main/java/teamsmartphone1/com/tvent/EventList.java
@@ -6,21 +6,54 @@ import android.util.Log;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.HashSet;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 /**
  * Created by Bruce on 9/21/2016.
  */
 public class EventList {
     private static HashSet<Event> event_set;
+    private static boolean refresh;
 
     public EventList() {
         super();
     }
 
     public EventList(LatLng loc) {
+        if (refresh) return;
         String req = String.format("http://domain/v1/data/events?latlng=%f%%2C%f", loc.latitude, loc.longitude);
         Log.d("TVENT", req);
-        return;
-        //HTTPRequest request = new HTTPRequest();
+        String res = HTTPRequest.sendRequest(req);
+
+        JSONObject response = null;
+        try {
+            response = new JSONObject(res);
+        } catch (JSONException e) {
+            Log.d("EVENT_LIST", "Failed to get JSON object");
+        }
+        assert response != null;
+        //now get the array of "movies"data
+        JSONArray events = response.optJSONArray("events");
+        for (int i = 0; i < events.length(); i++) {
+            try {
+                JSONObject event = events.getJSONObject(i);
+                assert event != null;
+                int id= event.optInt("id");
+                HashSet<Tweet> tweet_set = new HashSet<Tweet>();
+                JSONArray tweets = event.optJSONArray("tweets");
+                for (int j = 0; j < tweets.length(); j++) {
+                    tweet_set.add((Tweet)tweets.opt(j));
+                }
+                event_set.add(new Event(id, loc, tweet_set));
+            } catch (JSONException e) {
+                Log.d("EVENT_LIST", "Failed to get JSON object");
+            }
+        }
+        refresh = false;
     }
+
+    public void need_refresh() { refresh = true; }
 }
