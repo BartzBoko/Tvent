80c0a607acf66b0abac80495cc997c21392dc89d
dchudy3 <dchudy@gatech.edu>
2016-10-14 14:45:11 -0400

    Eventconnecter, connected to APi

--- app/src/main/java/teamsmartphone1/com/tvent/EventServerConnector.java
+++ app/src/main/java/teamsmartphone1/com/tvent/EventServerConnector.java
@@ -9,54 +9,66 @@ import org.json.JSONObject;
 import com.google.android.gms.maps.model.LatLng;
 
 public class EventServerConnector {
-    String URL_ENDPOINT = "http://domain";
+    String URL_ENDPOINT = "http://54.191.15.168";
     int EVENTCOUNT = 5;
     int TWEETCOUNT = 5;
-    boolean ISSERVERCONNECTED = true;
+    final int GETEVENTS = 1;
+    final int GETHEADERS = 2;
+    final int GETTWEETS = 3;
 
-    public HashSet<Event> getEvents(LatLng lat) {
-        // String request = URL_ENDPOINT + "/v2/data/events?latlng=" + latlng + "&count=" + EVENTCOUNT;
-        String request = "http://www.mockbin.org/bin/f8965994-347e-4117-9845-c4d02ed44b42?foo=bar&foo=baz";
+    public ArrayList<Event> getEvents(LatLng lat) {
 
-        HashSet<Event> events = new HashSet<>();
+        String request = URL_ENDPOINT + "/v2/data/events?latlng=35.0136831%2c-97.3611394&count=" + EVENTCOUNT;
+        if (lat != null) {
+            String latt = lat.toString().substring(lat.toString().indexOf("(")+1,lat.toString().indexOf(","));
+            String longg = lat.toString().substring(lat.toString().indexOf(",")+1,lat.toString().length()-1);
+            request = URL_ENDPOINT + "/v2/data/events?latlng=" + latt + "%2c" + longg + "&count=" + EVENTCOUNT;
+        }
+        //String request = "http://www.mockbin.org/bin/9f202603-2e7d-4ea2-b251-be444609beed?foo=bar&foo=baz";
+
+        ArrayList<Event> events = new ArrayList<Event>();
         String res = HTTPRequest.sendRequest(request);
-        //Log.d("ESC", res);;
-        try {
-            JSONObject  jsonRootObject = new JSONObject(res);
-            JSONArray jsonArray = jsonRootObject.optJSONArray("events");
-            for(int i=0; i < jsonArray.length(); i++){
-                int id  = jsonArray.getInt(i);
-                //Log.d("ESC", Integer.toString(id));
-                Event event = getEventHeaders(id);
-                events.add(event);
-            }
-        } catch (JSONException e) {e.printStackTrace();}
+        // Log.d("ESC", res);
+        HashSet<String> eventlist = JsonFixerEvent(res);
+        System.out.println(eventlist.size());
+        for(String id : eventlist){
+            System.out.println("dan => " + id);
+            Event event = getEventHeaders(id);
+            events.add(event);
+        }
+
         return events;
     }
 
-    private Event getEventHeaders(int eventid) {
-        // String request = URL_ENDPOINT + "/v2/data/event/" + eventid + ?&count=0";;
-        String request = "http://www.mockbin.org/bin/8454d0a9-fd40-40d0-ad1d-3598c999f2b6?foo=bar&foo=baz";
+    private Event getEventHeaders(String eventid) {
+        String request = URL_ENDPOINT + "/v2/data/event/" + eventid + "?count=1";
+        //String request = "http://www.mockbin.org/bin/1aea96b4-e063-48fb-98d6-27eebe47d715?foo=bar&foo=baz";
         String res = HTTPRequest.sendRequest(request);
-        //Log.d("ESC", res);
+        Log.d("ESC", res);
         Event returnevent = null;
+        res = (res.substring(0, res.lastIndexOf(",")) + "}");
+        Log.d("ESC", res);
         try {
             JSONObject  jsonRootObject = new JSONObject(res);
             String hashtag = jsonRootObject.getString("hashtag");
             Double lat = jsonRootObject.getDouble("lat");
             Double lng = jsonRootObject.getDouble("lng");
             LatLng ll = new LatLng(lat,lng);
-            returnevent = new Event(eventid, hashtag, ll, null);
-            //Log.d("ESC", hashtag);
+            int id = Integer.valueOf(eventid);
+            returnevent = new Event(id, hashtag, ll, null);
+            Log.d("ESC", hashtag);
         } catch (JSONException e) {e.printStackTrace();}
         return returnevent;
     }
 
     public HashSet<Tweet> getTweets(int eventid) {
-        //String request = URL_ENDPOINT + "/v2/data/event/" + eventid + "?&count=" + TWEETCOUNT;
-        String request = "http://www.mockbin.org/bin/429823d6-0af8-4ccd-91e7-795966eb81ef?foo=bar&foo=baz";
+        String request = URL_ENDPOINT + "/v2/data/event/" + eventid + "?&count=" + TWEETCOUNT;
+        //String request = "http://www.mockbin.org/bin/8f6f6b76-a253-49e2-ae09-5d063452d88d?foo=bar&foo=baz";
         String res = HTTPRequest.sendRequest(request);
-        //Log.d("ESC", res);
+        // Log.d("ESCDDDDDDDDD", res);
+        res = (res.substring(0, res.lastIndexOf("]")));
+        res = (res.substring(0, res.lastIndexOf("}") + 1) + "]}");
+        // Log.d("ESCDDDDDDDDD", res);
         HashSet<Tweet> tweets = new HashSet<Tweet>();
         JSONObject jsonRootObject = null;
         Tweet outtweet = null;
@@ -73,4 +85,21 @@ public class EventServerConnector {
         } catch (JSONException e) {e.printStackTrace();}
         return tweets;
     }
+
+    private HashSet<String> JsonFixerEvent(String poorJSON) {
+        HashSet<String> output = new HashSet<String>();
+        String returnstring = poorJSON.substring((poorJSON.indexOf( '[' ) + 1) ,(poorJSON.indexOf( ']' )));
+        returnstring = returnstring.replace(" ", "");
+        int count = returnstring.length() - returnstring.replace(",", "").length();
+        for (int x = 0; x < count; x++) {
+            output.add(returnstring.substring(0,returnstring.indexOf( ',' )) );
+            returnstring = returnstring.substring((returnstring.indexOf( ',') + 1),returnstring.length());
+        }
+        if (!returnstring.equals("")) {
+            output.add(returnstring);
+        }
+        return output;
+    }
+
+
 }
